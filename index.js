9db6db4adcfe434415837cd6812eeffd4fbba18f
kala <kalavathi.muddurthi@gmail.com>
2025-03-08 13:56:30 +0530

    s1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Service 1!');
+});
+
+app.listen(port, () => {
+  console.log(`Service 1 is running on http://localhost:${port}`);
+});
+
